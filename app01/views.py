3f66547607caaf354793cef160444212c6f24704
littleStone <1042536516@qq.com>
2020-10-13 10:35:17 +0800

    更新左侧导航栏，显示世界地图 Signed-off-by: littleStone <1042536516@qq.com>

--- app01/views.py
+++ app01/views.py
@@ -9,3 +9,13 @@ def index(request):
     msg_err = 'ok'
 
     return render(request, 'index.html', {'msg_err': msg_err, })
+
+
+def layout(request):
+    if request.method == 'GET':
+        pass
+    else:
+        pass
+    msg_err = 'ok'
+
+    return render(request, 'layout.html', {'msg_err': msg_err, })
